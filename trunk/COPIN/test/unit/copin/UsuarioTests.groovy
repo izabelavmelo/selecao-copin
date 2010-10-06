8522ecea9141d803bcb55670b433f4e83dc541da
izabela.vmelo <izabela.vmelo@b5fd5c40-a538-4538-c1a7-4a988369325a>
2010-10-06 12:41:20 +0000

    ...

--- trunk/COPIN/test/unit/copin/UsuarioTests.groovy
+++ trunk/COPIN/test/unit/copin/UsuarioTests.groovy
@@ -3,15 +3,49 @@ package copin
 import grails.test.*
 
 class UsuarioTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
-
-    protected void tearDown() {
-        super.tearDown()
-    }
-
-    void testSomething() {
-
-    }
+	
+	void testCreate() {
+		def u1 = new Usuario( //eh cadastrado
+				login:'teste',
+				senha :'teste123',
+				nome : 'Joaquim',
+				email : 'joaquim@teste.com',
+				cpf : '08580911439');
+		def u2 = new Usuario( //nao eh cadastrado por causa de senha
+				login:'teste1',
+				senha :'tes',
+				nome : 'Joao',
+				email : 'joaquim@teste.com',
+				cpf : '08580911438');
+		def u3 = new Usuario(
+				login:'teste2',
+				senha :'teste1234',
+				nome : 'Joaquim',
+				email : 'joaquimohhh@teste.com',
+				cpf : '08580911437');
+		def u4 = new Usuario( //nao eh cadastrado: falta item de email
+				login:'teste3',
+				senha :'teste1234',
+				nome : 'Joaquim',
+				email : '',
+				cpf : '08580911436');
+		
+		
+		assertEquals u1.getNome(), "Joaquim"
+		//assertEquals u2.getNome(), "Joao"
+		assertEquals u3.getNome(), "Joaquim"
+		//assertEquals u4.getNome(), "Joaquim"
+		
+		assertEquals u1.getLogin(), "teste"
+		//assertEquals u2.getLogin(), "teste1"
+		assertEquals u3.getLogin(), "teste2"
+		//assertEquals u4.getLogin(), "teste3"
+		
+		/*assertEquals u1.getPasswd(), "senhasemcriptografia"
+		assertEquals u2.getPasswd(), "teste2"
+		assertEquals u3.getPasswd(), "senhaaa"
+		
+		assertTrue u1.isEnabled()
+		assertFalse u2.isEnabled()**/
+	}
 }
